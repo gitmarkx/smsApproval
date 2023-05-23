26fabe43ed0d1d39df875b4307a97754fdc231b1
Mark Personal <markarielmaata@gmail.com>
2023-05-23 13:20:09 +0800

    change layout/app ui layout and sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
         <!-- Page Content -->
         <div class="p-4 sm:ml-64">
             <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                
+                @yield('content')
             </div>
         </div>
     </div>
